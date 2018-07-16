f163a260ee577fc932486fb43bd5753c31699d57
Blandine <Adminl@EMER05>
2018-07-16 17:01:17 +0200

    Fin de journée 16/07

---
+++ Roomy/Roomy/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Roomy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Roomy.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View();
+        }
+    }
+}
---
+++ Roomy/Roomy/Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Roomy.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        [Display(Name = "Nom")]
+        [StringLength(50, MinimumLength = 2,
+            ErrorMessage = "Le champ {0} doit contenir entre {2} et {1} caractères.")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        [Display(Name = "Prénom")]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        [Display(Name = "Email")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                           @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                           @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+            ErrorMessage = "L'adresse mail n'est pas au bon format.")]
+        public string Mail { get; set; }
+
+        [Display(Name = "Date de naissance")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        [Display(Name = "Mot de passe")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{6,}$",
+            ErrorMessage = "{0} incorrect.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "La confirmation du mot de passe est requise.")]
+        [Display(Name = "Confirmer le mot de passe")]
+        [Compare("Password", ErrorMessage = "Erreur sur la confirmation du mot de passe.")]
+        [DataType(DataType.Password)]
+        public string ConfirmedPassword { get; set; }
+    }
+}
